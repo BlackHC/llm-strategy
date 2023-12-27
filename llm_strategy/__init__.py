1d531767230843cbc77ffc40cd1a04a428b38ceb
Andreas Kirsch <blackhc@gmail.com>
2023-12-27 20:03:24 +0100

    Bump version: 2.1.0 → 2.1.1

--- llm_strategy/__init__.py
+++ llm_strategy/__init__.py
@@ -4,4 +4,4 @@ from llm_strategy.llm_function import (  # type: ignore
 )
 from llm_strategy.llm_strategy import llm_dataclass, llm_strategy  # type: ignore
 
-__version__ = '2.1.0'
+__version__ = '2.1.1'
